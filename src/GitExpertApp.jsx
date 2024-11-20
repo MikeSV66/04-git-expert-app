cfa625f138432ffdcad057cdd50e9341baca3921
Miguel Guirola <115573201+MikeSV66@users.noreply.github.com>
2024-11-20 12:14:39 -0600

    Add files via upload

---
+++ src/GitExpertApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { AddCategory}  from './components/AddCategory'
+import { GifGrid } from "./components/GifGrid";
+
+
+export const GitExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch' ]);
+    
+    const onAddCategory = ( newCategory ) => {
+        
+        if( categories.includes(newCategory) ) return;
+        
+        setCategories( [ newCategory, ...categories ] );
+    }
+
+    return (
+        <>
+            <h1>GitExpertApp</h1>
+
+        <AddCategory 
+            //setCategories={setCategories}
+            onNewCategory={ (value) => onAddCategory(value) }
+        />
+       
+            {
+                categories.map( ( category ) => (
+                    <GifGrid 
+                        key= { category } 
+                        category={ category }/> 
+                ))  
+            }
+
+        </> 
+    )
+}
